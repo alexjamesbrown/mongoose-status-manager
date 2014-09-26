1432b3a2caa683ae8f1f3e451a98c58896bafbdf
Alex Brown <alex@alexjamesbrown.com>
2014-09-26 12:25:07 +0100

    check can save and retrieve our doc to db and test status and status_updates added status to schema

--- test/mongoose-status-manager.js
+++ test/mongoose-status-manager.js
@@ -1,7 +1,7 @@
 var mocha = require('mocha'),
-    should = require('should'),
-    db = require('mongoose'),
-    statusPlugin = require('../lib/mongoose-status-manager');
+should = require('should'),
+db = require('mongoose'),
+statusPlugin = require('../lib/mongoose-status-manager');
 
 describe('Mongoose Status Manager', function(){
 
@@ -31,7 +31,7 @@ describe('Mongoose Status Manager', function(){
         it('sets status', function(){
 
         	doc.updateStatus('first status')
-            
+
             should.exist(doc.status, 'status not set')
             doc.status.should.equal('first status')
         });
@@ -79,5 +79,29 @@ describe('Mongoose Status Manager', function(){
             doc.status_updates[0].meta.userId.should.equal('abc123')
             doc.status_updates[0].meta.otherUserId.should.equal('xyz789')
         });
+
+        it('saves and retrieves from database', function(done){
+         doc.updateStatus('first status')
+         doc.updateStatus('second status', {userId: 1234})
+
+         doc.save(function(err){
+            if(err) done(err);
+
+            //retrieve from db
+            Order.find(function(err, result){
+                var order = result[0];
+                
+                order.status.should.equal('second status');
+
+                order.status_updates.should.have.length(2);
+                order.status_updates[0].status.should.equal('second status');
+                order.status_updates[1].status.should.equal('first status');
+
+                order.status_updates[0].meta.userId.should.equal(1234);
+
+                done();
+            });
+         });
+     });
     });
 });
